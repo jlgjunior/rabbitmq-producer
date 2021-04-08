abb2476bb53690c60fb2d00d7d2e8b1c5c7241c5
jlgjunior <jgaribaldijunior@gmail.com>
2021-04-08 16:33:29 -0300

    Add every file

---
+++ src/test/java/com/course/rabbiqmqproducer/RabbiqmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.rabbiqmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbiqmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
