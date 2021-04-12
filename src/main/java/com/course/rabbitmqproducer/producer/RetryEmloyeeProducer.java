525d39fd0ae258084f79fa396be918c658098e6b
jlgjunior <jgaribaldijunior@gmail.com>
2021-04-12 13:15:49 -0300

    Produce producing invalid employees for testing retry mechanism

--- src/main/java/com/course/rabbitmqproducer/producer/RetryEmloyeeProducer.java
+++ src/main/java/com/course/rabbitmqproducer/producer/RetryEmloyeeProducer.java
@@ -4,21 +4,20 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.course.rabbitmqproducer.entity.Picture;
+import com.course.rabbitmqproducer.entity.Employee;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-//@Service
-public class RetryPictureProducer {
+@Service
+public class RetryEmloyeeProducer {
 
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 	private ObjectMapper objectMapper = new ObjectMapper();
 	
-	public void sendMessage(Picture picture) throws JsonProcessingException {
-		String pictureJson  = objectMapper.writeValueAsString(picture);
-		
-		rabbitTemplate.convertAndSend("x.guideline.work", picture.getType(), pictureJson);
-	}
 	
+	public void sendMessage(Employee employee) throws JsonProcessingException {
+		String employeeJson = objectMapper.writeValueAsString(employee);
+		rabbitTemplate.convertAndSend("x.guideline2.work", "", employeeJson);
+	}
 }
